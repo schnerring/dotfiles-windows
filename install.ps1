812402c0f163ee0c2c78f6b71e883c6382d21472
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2022-07-20 00:12:53 +0200

    Add more winget packages

--- install.ps1
+++ install.ps1
@@ -35,3 +35,12 @@ $packages = @(
 foreach ($package in $packages) {
     winget install --silent --source winget --accept-source-agreements --accept-package-agreements "$package"
 }
+
+
+winget install --silent --source winget --accept-source-agreements --accept-package-agreements "Microsoft.PowerShell" --override "ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1"
+winget install --silent --source winget --accept-source-agreements --accept-package-agreements "TeamViewer.TeamViewer"
+winget install --silent --source winget --accept-source-agreements --accept-package-agreements "SumatraPDF.SumatraPDF"
+
+winget install --silent --source winget --accept-source-agreements --accept-package-agreements "Microsoft.Office"
+winget install --silent --source winget --accept-source-agreements --accept-package-agreements "Microsoft.VisualStudio.2022.Professional" --override "--add Microsoft.VisualStudio.Workload.Azure;includeRecommended --add Microsoft.VisualStudio.Workload.ManagedDesktop;includeRecommended --add Microsoft.VisualStudio.Workload.NetCrossPlat;includeRecommended --add Microsoft.VisualStudio.Workload.NetWeb;includeRecommended --add Microsoft.VisualStudio.Workload.Office;includeRecommended"
+
