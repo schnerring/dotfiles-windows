8cfe7fb20043b15916358319ab0b1fc543184346
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2022-09-10 01:02:11 +0200

    Move winget installations to separate script

---
+++ Install-WingetPackages.ps1
@@ -0,0 +1,112 @@
+#requires -RunAsAdministrator
+
+function Assert-WingetPackage {
+    param (
+        [string] $PackageId
+    )
+
+    $wingetList = winget list --id "$PackageId"
+
+    # Iterate over `winget list` output lines
+    foreach ($line in $wingetList) {
+        if ($line.Contains($PackageId)) {
+            return $true
+        }
+    }
+
+    return $false
+}
+
+function Install-WingetPackage {
+    param (
+        [Parameter(Position = 0)]
+        [string] $PackageId,
+
+        [Parameter(Position = 1)]
+        [string] $PackageOverride
+    )
+    
+    if (Assert-WingetPackage $PackageId) {
+        Write-Output "Skipping $($PackageId) because it is already installed..."
+        return
+    }
+
+    Write-Output "Installing $($PackageId)..."
+
+    if ($PackageOverride) {
+        winget install --silent --source winget --accept-source-agreements --accept-package-agreements $PackageId --override $PackageOverride
+    } else {
+        winget install --silent --source winget --accept-source-agreements --accept-package-agreements $PackageId
+    }
+}
+
+# Browsers
+Install-WingetPackage "Mozilla.Firefox"
+Install-WingetPackage "Google.Chrome"
+    
+# Common
+Install-WingetPackage "7zip.7zip"
+Install-WingetPackage "DownloadHelper.VdhCoApp"
+Install-WingetPackage "KeePassXCTeam.KeePassXC"
+Install-WingetPackage "Microsoft.Office"
+Install-WingetPackage "OBSProject.OBSStudio"
+Install-WingetPackage "PDFsam.PDFsam"
+Install-WingetPackage "REALiX.HWiNFO"
+Install-WingetPackage "SatoshiLabs.trezor-suite"
+#Install-WingetPackage "Spotify.Spotify" TODO: run in non-admin shell
+Install-WingetPackage "SumatraPDF.SumatraPDF"
+Install-WingetPackage "Valve.Steam"
+Install-WingetPackage "VideoLAN.VLC"
+
+# Development
+Install-WingetPackage "Git.Git"
+Install-WingetPackage "GoLang.Go.1.19"
+Install-WingetPackage "GnuPG.Gpg4win"
+Install-WingetPackage "Microsoft.AzureCLI"
+Install-WingetPackage "Microsoft.AzureStorageExplorer"
+Install-WingetPackage "Microsoft.PowerShell" "ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1"
+Install-WingetPackage "Microsoft.RemoteDesktopClient"
+Install-WingetPackage "Microsoft.SQLServerManagementStudio"
+Install-WingetPackage "Microsoft.VisualStudio.2022.Professional" "--add Microsoft.VisualStudio.Workload.Azure;includeRecommended --add Microsoft.VisualStudio.Workload.ManagedDesktop;includeRecommended --add Microsoft.VisualStudio.Workload.NetCrossPlat;includeRecommended --add Microsoft.VisualStudio.Workload.NetWeb;includeRecommended --add Microsoft.VisualStudio.Workload.Office;includeRecommended"
+Install-WingetPackage "Microsoft.VisualStudioCode" '/SILENT /mergetasks="!runcode,addcontextmenufiles,addcontextmenufolders"'
+Install-WingetPackage "Notepad++.Notepad++"
+Install-WingetPackage "OpenJS.NodeJS.LTS"
+Install-WingetPackage "Postman.Postman"
+Install-WingetPackage "PuTTY.PuTTY"
+Install-WingetPackage "Python.Python.3.10"
+
+# Communications
+Install-WingetPackage "Discord.Discord"
+Install-WingetPackage "Element.Element"
+Install-WingetPackage "HexChat.HexChat"
+Install-WingetPackage "Mozilla.Thunderbird"
+Install-WingetPackage "OpenWhisperSystems.Signal"
+Install-WingetPackage "ProtonTechnologies.ProtonMailBridge"
+Install-WingetPackage "TeamSpeakSystems.TeamSpeakClient"
+Install-WingetPackage "TeamViewer.TeamViewer"
+
+# VPN
+Install-WingetPackage "MullvadVPN.MullvadVPN"
+Install-WingetPackage "OpenVPNTechnologies.OpenVPN"
+Install-WingetPackage "WireGuard.WireGuard"
+
+# Jellyfin
+Install-WingetPackage "Jellyfin.JellyfinMediaPlayer"
+Install-WingetPackage "IanWalton.JellyfinMPVShim"
+Install-WingetPackage "jeffvli.Sonixd"
+
+# Graphics Editors
+Install-WingetPackage "JGraph.Draw"
+Install-WingetPackage "Inkscape.Inkscape"
+
+# YubiKey
+Install-WingetPackage "Yubico.YubikeyManager"
+Install-WingetPackage "Yubico.Authenticator"
+
+# Audio
+Install-WingetPackage "Audacity.Audacity"
+Install-WingetPackage "AlexanderKojevnikov.Spek"
+Install-WingetPackage "AndreWiethoff.ExactAudioCopy"
+Install-WingetPackage "MediaArea.MediaInfo.GUI"
+Install-WingetPackage "ChrisBagwell.SoX"
+Install-WingetPackage "PeterPawlowski.foobar2000"
--- install.ps1
+++ install.ps1
@@ -9,85 +9,10 @@ Invoke-WebRequest -UseBasicParsing "https://community.chocolatey.org/install.ps1
 # Install Scoop (Admin not necessary)
 Invoke-WebRequest -UseBasicParsing get.scoop.sh | Invoke-Expression
 
-$packages = @(
-    # Browsers
-    "Mozilla.Firefox", # moniker: firefox
-    "Google.Chrome",   # moniker: chrome
-
-    # Common
-    "7zip.7zip",
-    "KeePassXCTeam.KeePassXC",
-    "VideoLAN.VLC",    # moniker: vlc
-
-    # Development
-    "Notepad++.Notepad++",
-    "Postman.Postman",
-
-    # Gaming
-    "Valve.Steam",
-    "TeamSpeakSystems.TeamSpeakClient", # moniker: teamspeak
-
-    # Misc
-    "REALiX.HWiNFO",
-
-    # Graphics Editors
-    "JGraph.Draw",
-    "Inkscape.Inkscape",
-
-    # YubiKey
-    "Yubico.YubikeyManager",
-    "Yubico.Authenticator"
-)
-
-foreach ($package in $packages) {
-    winget install --silent --source winget --accept-source-agreements --accept-package-agreements "$package"
-}
-
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "Python.Python.3"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "Microsoft.PowerShell" --override "ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "TeamViewer.TeamViewer"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "SumatraPDF.SumatraPDF"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "Microsoft.Office"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "Microsoft.VisualStudio.2022.Professional" --override "--add Microsoft.VisualStudio.Workload.Azure;includeRecommended --add Microsoft.VisualStudio.Workload.ManagedDesktop;includeRecommended --add Microsoft.VisualStudio.Workload.NetCrossPlat;includeRecommended --add Microsoft.VisualStudio.Workload.NetWeb;includeRecommended --add Microsoft.VisualStudio.Workload.Office;includeRecommended"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "Microsoft.AzureStorageExplorer"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "Microsoft.SQLServerManagementStudio"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "Mozilla.Thunderbird"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "OpenWhisperSystems.Signal"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "Element.Element"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "ProtonTechnologies.ProtonMailBridge"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "MullvadVPN.MullvadVPN"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "WireGuard.WireGuard"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "GnuPG.Gpg4win"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "Microsoft.RemoteDesktopClient"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "PuTTY.PuTTY"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "OpenJS.NodeJS.LTS"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "OpenVPNTechnologies.OpenVPN"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "PDFsam.PDFsam"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "DownloadHelper.VdhCoApp"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "Microsoft.AzureCLI"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "OBSProject.OBSStudio"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "HexChat.HexChat"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "Microsoft.VisualStudioCode" --override '/SILENT /mergetasks="!runcode,addcontextmenufiles,addcontextmenufolders"'
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "SatoshiLabs.trezor-suite"
-
-# Audio
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "Audacity.Audacity"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "AlexanderKojevnikov.Spek"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "AndreWiethoff.ExactAudioCopy"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "MediaArea.MediaInfo.GUI"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "ChrisBagwell.SoX"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "PeterPawlowski.foobar2000"
 choco install --yes lossless-audio-checker
 choco install --yes lame
 
-# Jellyfin
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "Jellyfin.JellyfinMediaPlayer"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "IanWalton.JellyfinMPVShim"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "jeffvli.Sonixd"
-
 # TODO: don't run as admin
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "Spotify.Spotify"
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "Discord.Discord"
 npm install --global npm-check-updates
 
 scoop install intermodal
@@ -108,7 +33,6 @@ choco install --yes kubernetes-helm
 choco install --yes paint.net
 
 # Hugo
-winget install --silent --source winget --accept-source-agreements --accept-package-agreements "GoLang.Go"
 choco install --yes hugo-extended
 
 # gpg4win + win ssh + wsl
